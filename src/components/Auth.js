63a934bce8e3f6a42657e6c65fe8c9962a7e421b
Prashant Raj <pr2125116@gmail.com>
2025-07-08 17:47:46 +0530

    Update

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,22 +1,28 @@
-import React, { useContext } from 'react'
-import { AuthUserContext } from './context/AuthContext';
+import React, { useContext } from "react";
+import { AuthUserContext } from "./context/AuthContext";
 
 const Auth = () => {
-  const {user, setUser} = useContext(AuthUserContext);
+	const { user, setUser } = useContext(AuthUserContext);
 
-  return (
-    <div>
-      {!user ? <p>you are not authenticated</p>
-      : <p>You are now authenticated, you can proceed</p>}
-      <label>
-        <input type='checkbox'
-        checked={user}
-        onChange={() => setUser(prev => !prev)}
-        />
-        I'm not a robot
-      </label>
-    </div>
-  )
-}
+	return (
+		<div>
+			{!user ? (
+				<p>you are not authenticated</p>
+			) : (
+				<p className="authText">
+					You are now authenticated, you can proceed
+				</p>
+			)}
+			<label>
+				<input
+					type="checkbox"
+					checked={user}
+					onChange={() => setUser((prev) => !prev)}
+				/>
+				I'm not a robot
+			</label>
+		</div>
+	);
+};
 
-export default Auth
+export default Auth;
